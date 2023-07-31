4645d02a52c295ad07fa137aa0c990f8cc22d763
Juan Chung <jcdev@Juans-MacBook-Pro.local>
2023-07-31 15:53:26 -0700

    Updated Counter function

--- pages/component/VisitorCounter.js
+++ pages/component/VisitorCounter.js
@@ -1,15 +1,23 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 export default function VisitorCounter() {
-    const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
+  const [error, setError] = useState(null);
 
-    useEffect(() => {
-        fetch('/api/counter')
-        .then((res) => res.json())
-        .then((data) => setCount(data.count));
-    }, []);
+  useEffect(() => {
+    fetch("https://x3tsc8hfmg.execute-api.us-east-1.amazonaws.com/prod/mysite")
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Response was not ok");
+        }
+        return res.json();
+      })
+      .then((data) => setCount(data.count))
+      .catch((error) => setError(error.message));
+  }, []);
 
-    return (
-        <h1 className="text-gray-600">Visits: {count} </h1>
-    );
-};
+  if (error) {
+    return <h1 className="text-red-600">Error: {error}</h1>;
+  }
+  return <h1 className="text-gray-600">Visits: {count} </h1>;
+}
